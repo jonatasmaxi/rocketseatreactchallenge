26bb9fe5847af89363a13a8c37e06afffda3de36
Jonatas Maximiano <maximianojonatas@gmail.com>
2020-08-24 15:04:25 -0300

    Add files from challenge

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -15,7 +15,7 @@ import api from '../../services/api';
 interface FileProps {
   file: File;
   name: string;
-  readableSize: string;
+  readableSize: number;
 }
 
 const Import: React.FC = () => {
@@ -23,19 +23,26 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    const { file }  = uploadedFiles[0];
+    data.append('file',file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const uploadFiles : FileProps[] = files.map((file) => ({
+      file,
+      name: file.name, 
+      readableSize: file.size
+    }));
+
+    setUploadedFiles(uploadFiles);
   }
 
   return (
